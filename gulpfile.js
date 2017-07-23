bf49ada0756ee23063567bb394d53f4b97ecb058
shadid12 <shadidhaque2014@gmail.com>
2017-07-23 04:15:26 -0400

    gulp sass done

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,30 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync').create();
 const nodemon 	  = require('gulp-nodemon');
-
+const sass 		  = require('gulp-sass');
 
 gulp.task('default', ['browser-sync'], function () {
 });
 
-gulp.task('browser-sync', ['nodemon'], function() {
+gulp.task('browser-sync', function() {
 	browserSync.init(null, {
 		proxy: "http://localhost:5000",
-        files: ["public/**/*.*", "views/**/*"],
+        files: ["public/**/*.*", "views/*"],
         port: 7000,
 	});
 });
 
+
+gulp.task('styles', function() {
+    gulp.src('./public/assets/css/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./views',['browser-sync']));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('./public/assets/css/*', ['styles'])
+	gulp.watch('./public/assets/css/*.css').pipe(browserSync.reload())
+});
 
 gulp.task('nodemon', function (cb) {
 	
@@ -28,3 +39,5 @@ gulp.task('nodemon', function (cb) {
 		} 
 	});
 });
+
+gulp.task('default', ['browser-sync', 'watch']);
